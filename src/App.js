3dd123561d0b47fce9946d2720d01b9080b5e019
OlgaKu <olgakuzmina-web@yandex.ru>
2019-12-05 13:29:13 +0300

    added Dialogs, edited slyles of App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import classes from '../src/App.module.css';
+import '../src/App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import Dialog from './components/Dialogs/Dialogs';
 
 
 
 
 const App = () => {
   return (
-    <div className={classes.App}>
+    <div className="app">
       <Header />
-      <div className={classes.wrapper}>
+      <div className="app-wrapper">
         <Sidebar />
-        <Profile />
+        <div className="app-wrapper-content">
+          {/* <Dialog /> */}
+          <Profile />
+        </div>
+
       </div>
     </div>
   );
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Dialogs.module.css';
+
+const Dialog = () => {
+    return (
+        <section className={classes.dialogs}>
+            Lets go!
+    </section>
+    );
+}
+
+export default Dialog;
