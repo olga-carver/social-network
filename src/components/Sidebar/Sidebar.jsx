e4640831e5a12e40e8e1a0e10cf718ef9f144931
OlgaKu <olgakuzmina-web@yandex.ru>
2019-12-06 01:16:47 +0300

    created working menu in Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import classes from '../Sidebar/Sidebar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Sidebar = () => {
     return (
 <nav className={classes.sidebar}>
         <ul>
-          <li><a href="/news">News</a></li>
-          <li><a href="/profile">Profile</a></li>
-          <li><a href="/dialogs">Dialogs</a></li>
-          <li><a href="/friends">Settings</a></li>
+          <li><NavLink to="/news" activeClassName={classes.activeLink}>News</NavLink></li>
+          <li><NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink></li>
+          <li><NavLink to="/dialogs" activeClassName={classes.activeLink}>Dialogs</NavLink></li>
+          <li><NavLink to="/friends" activeClassName={classes.activeLink}>Settings</NavLink></li>
         </ul>
       </nav>
     );
